25178ada51c64d7cc6f36dc56c918ec8033a83f7
Marco Nieves <nievesmarcoantonio16@gmail.com>
2024-04-26 10:06:39 -0400

    Added auth with some bugs

--- mapproject/map/urls.py
+++ mapproject/map/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.map, name="map"),
+    path("", views.map_view, name="map"),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
 ]
--- mapproject/map/views.py
+++ mapproject/map/views.py
@@ -1,4 +1,7 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
 from .models import Search
 from .forms import SearchForm
 import folium
@@ -6,7 +9,8 @@ import geocoder
 
 
 # Create your views here.
-def map(request):
+@login_required
+def map_view(request):
     if request.method == "POST":
         form = SearchForm(request.POST)
         if form.is_valid():
@@ -22,7 +26,7 @@ def map(request):
     if lat == None or lng == None:
         address.delete()
     # Create Map Object
-    m = folium.Map(location=[19, -12], zoom_start=2)
+    m = folium.Map(location=[38, 10], zoom_start=2.4)
     folium.Marker([lat, lng], tooltip="Location", popup=country).add_to(m)
     # Get HTML Presentation
     m = m._repr_html_()
@@ -31,3 +35,24 @@ def map(request):
         "form": form,
     }
     return render(request, "map.html", context)
+
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect("/")
+    else:
+        form = AuthenticationForm()
+    context = {"form": form}
+    return render(request, "login.html", context)
+
+
+def logout_view(request):
+    logout(request)
+    return redirect("")
